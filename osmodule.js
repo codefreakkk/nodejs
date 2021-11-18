cc53e6f059a5a52c132afbd142584def94d681ba
Harsh <harshsaid558@gmail.com>
2021-11-18 22:23:47 +0530

    new concepts

---
+++ osmodule.js
@@ -0,0 +1,8 @@
+const os = require("os");
+
+// console.log(os.arch());
+// console.log(os.hostname());
+// console.log(os.type());
+// console.log(os.userInfo());
+// const mem = os.freemem();
+// console.log(mem / 1024 / 1024 / 1024);
---
+++ path.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+// get directory name
+// console.log(path.dirname("E:/nodejs/path.js"));
+// get extension name
+// console.log(path.extname("E:/nodejs/path.js"));
+// get file name
+// console.log(path.basename("E:/nodejs/path.js"));
+
+// returns all the info of the file
+// console.log(path.parse("E:/nodejs/path.js"));
+
+// const parse = path.parse("E:/nodejs/path.js");
+// console.log(parse.root);
+
+// const newpath = path.join("foo", "bazar/", "l");
+// console.log(newpath);
