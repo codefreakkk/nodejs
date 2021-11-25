28d896736d07d02e90c3ff8c6166f650ee466ddb
Harsh <harshsaid558@gmail.com>
2021-11-25 10:45:25 +0530

    harsh

--- expressjs/server.js
+++ expressjs/server.js
@@ -1,15 +1,41 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+const hbs = require("hbs");
 
+// const temppath = path.join(__dirname, "./clientscripts");
+const partialspath = path.join(__dirname, "./partials");
+
+// set the template engine to hbs
 app.set("view engine", "hbs");
+// changning views folder name to clientscripts
+// app.set("views", temppath);
+hbs.registerPartials(partialspath);
 
 app.get("/", (req, res) => {
     res.render("index", {
         uname: "Harsh Sachin Said",
     });
 });
+
 app.get("/about", (req, res) => {
-    res.render("about");
+    res.render("about", {
+        uname: "Harsh sachin",
+    });
+});
+
+app.get("/about/*", (req, res) => {
+    res.render("error", {
+        errorComment: "Opps 404 Page not found",
+        uname: "Harsh sachin said",
+    });
+});
+
+app.get("*", (req, res) => {
+    res.render("error", {
+        errorComment: "Opps 404 Page not found",
+        uname: "Harsh sachin said",
+    });
 });
 
 app.listen(8000, () => {
--- stream.js
+++ stream.js
@@ -2,20 +2,20 @@ const fs = require("fs");
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-    // const rstream = fs.createReadStream("read.txt");
-    // rstream.on("data", (data) => {
-    //     res.write(data);
-    // });
-    // rstream.on("end", () => {
-    //     res.end();
-    // });
-    // rstream.on("error", () => {
-    //     res.end("Error");
-    // });
+    const rstream = fs.createReadStream("read.txt");
+    rstream.on("data", (data) => {
+        res.write(data);
+    });
+    rstream.on("end", () => {
+        res.end();
+    });
+    rstream.on("error", () => {
+        res.end("Error");
+    });
 
     // pipe method
-    const rstream = fs.createReadStream("read.txt");
-    rstream.pipe(res);
+    // const rstream = fs.createReadStream("read.txt");
+    // rstream.pipe(res);
 });
 
 server.listen(8000, () => {
